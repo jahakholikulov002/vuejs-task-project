19c41b0bf0b1a7a49f96e2837d43e30d99a235ef
jakhongirkholikulov <98052598+jahakholikulov002@users.noreply.github.com>
2022-10-10 19:49:43 +0500

    End of the project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add' " color="green"/>
+        <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add' " :color="showAddTask ? 'crimson' : 'green' "/>
     </header>
 </template>
 
